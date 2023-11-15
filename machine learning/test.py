8f96b67ad058319f5ca1d83fc673df974daa678a
AnujVerma <anujlaverma@gmail.com>
2023-11-14 21:11:10 -0600

    epoch change 300 to 100

--- machine learning/test.py
+++ machine learning/test.py
@@ -195,4 +195,4 @@ optimizer_conv = optim.SGD(model_conv.fc.parameters(), lr=0.001, momentum=0.9)
 exp_lr_scheduler = lr_scheduler.StepLR(optimizer_conv, step_size=7, gamma=0.1)
 
 model_conv = train_model(model_conv, criterion, optimizer_conv,
-                         exp_lr_scheduler, num_epochs=300)
+                         exp_lr_scheduler, num_epochs=100)
